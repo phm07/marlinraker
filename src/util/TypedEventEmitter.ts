5e03b5ce13a3a6b99b4701848860461d5bb54194
pauhull <pauhull3@gmail.com>
2022-10-28 21:47:28 +0200

    TypedEventEmitter implement all methods

--- src/util/TypedEventEmitter.ts
+++ src/util/TypedEventEmitter.ts
@@ -3,11 +3,24 @@ import EventEmitter from "events";
 type TEvents<E> = Record<keyof E, (...args: any[]) => void>;
 
 declare interface TypedEventEmitter<E extends TEvents<E>> {
-    on: <T extends keyof E>(event: T, listener: E[T]) => this;
-    emit: <T extends keyof E>(event: T, ...args: Parameters<E[T]>) => boolean;
+    addListener: <T extends keyof E>(eventName: T, listener: E[T]) => this;
+    emit: <T extends keyof E>(eventName: T, ...args: Parameters<E[T]>) => boolean;
+    eventNames: () => (keyof E)[];
+    getMaxListeners: () => number;
+    listenerCount: (eventName: keyof E) => number;
+    listeners: <T extends keyof E>(eventName: T) => E[T][];
+    off: <T extends keyof E>(eventName: T, listener: E[T]) => this;
+    on: <T extends keyof E>(eventName: T, listener: E[T]) => this;
+    once: <T extends keyof E>(eventName: T, listener: E[T]) => this;
+    prependListener: <T extends keyof E>(eventName: T, listener: E[T]) => this;
+    prependOnceListener: <T extends keyof E>(eventName: T, listener: E[T]) => this;
+    rawListeners: (eventName: keyof E) => (...args: any[]) => any;
+    removeAllListeners: (eventName?: keyof E) => this;
+    removeListener: <T extends keyof E>(eventName: T, listener: E[T]) => this;
+    setMaxListeners: (n: number) => this;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-extra-parens
-class TypedEventEmitter<E extends TEvents<E>> extends (EventEmitter as any) {}
+class TypedEventEmitter<E extends TEvents<E>> extends (EventEmitter as new() => {}) {}
 
 export default TypedEventEmitter;
