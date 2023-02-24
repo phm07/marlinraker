223d6e97eee9fce9779955f2381ce6c4660ff3b2
pauhull <pauhull3@gmail.com>
2023-02-24 23:50:01 +0100

    fix(serial): Don't crash when udevadm isn't found

--- src/util/SerialPortSearch.ts
+++ src/util/SerialPortSearch.ts
@@ -31,28 +31,33 @@ class SerialPortSearch {
         await this.database.deleteItem("marlinraker", "serial.last_port");
         await this.database.deleteItem("marlinraker", "serial.last_baud_rate");
 
-        const paths = (await SerialPort.list()).map((port) => port.path);
-        const baudRates = this.baudRate ? [this.baudRate] : [250000, 115200, 19200];
-        for (const path of paths) {
-            for (const baudRate of baudRates) {
-                logger.info(`Trying ${path} with baud rate ${baudRate}`);
+        try {
+            const paths = (await SerialPort.list()).map((port) => port.path);
 
-                let success = false;
-                try {
-                    success = await this.trySerialPort(path, baudRate);
-                } catch (e) {
-                    logger.error(e);
-                }
+            const baudRates = this.baudRate ? [this.baudRate] : [250000, 115200, 19200];
+            for (const path of paths) {
+                for (const baudRate of baudRates) {
+                    logger.info(`Trying ${path} with baud rate ${baudRate}`);
 
-                if (success) {
-                    logger.info("Success");
-                    await this.database.addItem("marlinraker", "serial.last_port", path);
-                    await this.database.addItem("marlinraker", "serial.last_baud_rate", baudRate);
-                    return [path, baudRate];
-                } else {
-                    logger.info("Cannot connect");
+                    let success = false;
+                    try {
+                        success = await this.trySerialPort(path, baudRate);
+                    } catch (e) {
+                        logger.error(e);
+                    }
+
+                    if (success) {
+                        logger.info("Success");
+                        await this.database.addItem("marlinraker", "serial.last_port", path);
+                        await this.database.addItem("marlinraker", "serial.last_baud_rate", baudRate);
+                        return [path, baudRate];
+                    } else {
+                        logger.info("Cannot connect");
+                    }
                 }
             }
+        } catch (e) {
+            logger.error(`Could not scan serial ports: ${e}`);
         }
 
         return null;
