171a7380a75bace3e59834128e4aaa645fbe83b7
pauhull <pauhull3@gmail.com>
2023-04-10 18:28:58 +0200

    chore: Use error code in response

--- src/api/MessageHandler.ts
+++ src/api/MessageHandler.ts
@@ -5,12 +5,14 @@ import { IMethodExecutor, TSender } from "./executors/IMethodExecutor";
 import { logger } from "../Server";
 
 abstract class MessageHandler {
+
     protected async handleMessage(sender: TSender, executor: IMethodExecutor<unknown, unknown> | undefined, params: unknown | undefined): Promise<Response> {
         if (!executor) {
             return new ErrorResponse(404, "Method not found");
         }
         let timeout: NodeJS.Timer | undefined;
         return await Promise.race<Response>([
+
             new Promise((resolve) => {
                 if (executor.timeout === null) return;
                 const ms = executor.timeout ?? 10000;
@@ -19,7 +21,16 @@ abstract class MessageHandler {
                     resolve(new ErrorResponse(408, "Request timeout"));
                 }, ms);
             }),
+
             new Promise((resolve) => {
+                const handleError = (e: unknown): void => {
+                    clearTimeout(timeout);
+                    const code = (e as { code?: number }).code ?? 500;
+                    const message = (e as { message?: string }).message ?? e?.toString();
+                    logger.error(`Error in ${executor.name}: ${message}`);
+                    resolve(new ErrorResponse(code, `Method error: ${e}`));
+                };
+
                 try {
                     Promise.resolve(executor.invoke(sender, (params ?? {}) as Partial<unknown>)).then((response) => {
                         clearTimeout(timeout);
@@ -28,15 +39,9 @@ abstract class MessageHandler {
                             resolve(new ErrorResponse(500, "No response"));
                         }
                         resolve(new ResultResponse<typeof response>(response));
-                    }).catch((e) => {
-                        clearTimeout(timeout);
-                        logger.error(`Error in ${executor.name}: ${e}`);
-                        resolve(new ErrorResponse(500, `Method error: ${e}`));
-                    });
+                    }).catch(handleError);
                 } catch (e) {
-                    clearTimeout(timeout);
-                    logger.error(`Error in ${executor.name}: ${e}`);
-                    resolve(new ErrorResponse(500, `Method error: ${e}`));
+                    handleError(e);
                 }
             })
         ]);
